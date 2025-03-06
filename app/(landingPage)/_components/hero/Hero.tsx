0d070c1021d9795f031e7aebb602839fe8e3979b
Code-X-Rahul <rr611305@gmail.com>
2025-03-06 14:11:29 +0530

    style: hero section

--- app/(landingPage)/_components/hero/Hero.tsx
+++ app/(landingPage)/_components/hero/Hero.tsx
@@ -7,27 +7,6 @@ import HeroCarousel from "./HeroCarousel";
 const Hero = () => {
   return (
     <LayoutWrapper className="relative">
-      <div className="grid place-items-center">
-        <Typography variant={"h1"} className="font-bold ">
-          Empowering{" "}
-          <span className="font-playfair italic text-[#436252]">
-            Zero-Waste
-          </span>
-        </Typography>
-        <Typography variant={"h1"} className="font-bold ">
-          Manufacturing
-        </Typography>
-      </div>
-      <Typography variant={"p"}>
-        Leverage real-time data and simulations to drive sustainable waste
-        reduction
-      </Typography>
-      <div className="flex items-center gap-5">
-        <Button size={"lg"}>Know More</Button>
-        <Button variant={"outline"} size={"lg"}>
-          Contact Us
-        </Button>
-      </div>
       <HeroCarousel />
     </LayoutWrapper>
   );
--- app/(landingPage)/_components/hero/HeroCarousel.tsx
+++ app/(landingPage)/_components/hero/HeroCarousel.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Carousel,
   CarouselContent,
@@ -5,18 +6,65 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Typography } from "@/components/ui/typography";
 import Image from "next/image";
 
 const IMAGES = [
   {
+    title: (
+      <div className="grid place-items-center">
+        <Typography variant={"h1"} className="font-bold max-w-prose">
+          <span className="font-playfair italic text-primary">
+            Close the Loop{" "}
+          </span>
+          – Optimize Efficiency
+        </Typography>
+      </div>
+    ),
+    description: (
+      <Typography variant={"p"}>
+        Leverage real-time data and simulations to drive sustainable waste
+        reduction.
+      </Typography>
+    ),
+
     src: "/hero.svg",
     alt: "Hero Icon",
   },
   {
+    title: (
+      <div className="grid place-items-center">
+        <Typography variant={"h1"} className="font-bold max-w-prose">
+          <span className="font-playfair italic text-primary">See Beyond </span>
+          – Gain Unparalleled Visibility
+        </Typography>
+      </div>
+    ),
+    description: (
+      <Typography variant={"p"}>
+        Automate in-depth mapping, track material flow, and monitor emissions
+        with AI-driven insights.
+      </Typography>
+    ),
     src: "/hero2.svg",
     alt: "Hero Icon",
   },
   {
+    title: (
+      <div className="grid place-items-center">
+        <Typography variant={"h1"} className="font-bold max-w-prose">
+          <span className="font-playfair italic text-primary">Stay Agile </span>
+          – Adapt to the Future
+        </Typography>
+      </div>
+    ),
+    description: (
+      <Typography variant={"p"}>
+        Automate in-depth mapping, track material flow, and monitor emissions
+        with AI-driven insights.
+      </Typography>
+    ),
+
     src: "/hero3.svg",
     alt: "Hero Icon",
   },
@@ -31,18 +79,28 @@ const HeroCarousel = () => {
       className="grid place-items-center  -translate-y-5 mx-auto"
     >
       <CarouselContent>
-        {IMAGES.map(({ src, alt }) => (
-          <CarouselItem key={src} className=" flex items-center justify-center">
-            <Image
-              width={0}
-              height={0}
-              loading="lazy"
-              fetchPriority="low"
-              layout="responsive"
-              src={src}
-              alt={alt}
-              className="max-w-[800px] max-h-[450px]"
-            />
+        {IMAGES.map(({ src, alt, title, description }) => (
+          <CarouselItem key={src} className="grid place-items-center gap-4">
+            {title}
+            {description}
+            <div className="flex items-center gap-5">
+              <Button size={"xl"}>Know More</Button>
+              <Button variant={"outline"} size={"xl"}>
+                Contact Us
+              </Button>
+            </div>
+            <div className="flex items-center justify-center">
+              <Image
+                width={0}
+                height={0}
+                loading="lazy"
+                fetchPriority="low"
+                layout="responsive"
+                src={src}
+                alt={alt}
+                className="max-w-[800px] max-h-[450px]"
+              />
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
