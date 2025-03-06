3068d0ea255ea56565e6c0c9b1feab3f1c79f48c
Code-X-Rahul <rr611305@gmail.com>
2025-03-06 14:11:04 +0530

    style: strategy section added

---
+++ app/(landingPage)/_components/strategy/Strategy.tsx
@@ -0,0 +1,119 @@
+import { Typography } from "@/components/ui/typography";
+import LayoutWrapper from "@/components/wrappers/LayoutWrapper";
+import Image from "next/image";
+import React from "react";
+
+const Strategy = () => {
+  return (
+    <div className="bg-primary-background">
+      <LayoutWrapper className="gap-5">
+        <div className="flex gap-2">
+          <Typography
+            variant={"h3"}
+            className="font-bold  text-primary-foreground"
+          >
+            Our strategic
+          </Typography>
+          <Typography
+            variant={"h3"}
+            className="font-playfair italic font-black  text-primary-foreground"
+          >
+            Partnerships & Achievements
+          </Typography>
+        </div>
+
+        <Typography
+          variant={"p"}
+          className="text-primary-foreground text-center max-w-[90%]"
+        >
+          Aavyooh helps manufacturers to map nth tier manufacturing networks in
+          a fast & transparent way providing unprecedented visibility into risks
+          and opportunities. Our advanced platform allows industries to
+          integrate physics based simulations and machine learning approaches
+          for transforming the manufacturing network towards sustainable
+          operations.
+        </Typography>
+
+        <div className="grid md:grid-cols-2 gap-5">
+          <div className="flex flex-col gap-5 bg-[#4A6758] px-10 pt-5 rounded-md overflow-hidden">
+            <Typography
+              variant={"h5"}
+              className="font-black font-playfair text-primary-foreground text-center"
+            >
+              R&D / Innovations
+            </Typography>
+            <ul className="list-disc">
+              <li className="text-primary-foreground leading-6 font-bold">
+                12+ Years of Cutting-Edge Research in Dr. Shweta Singh’s Lab
+              </li>
+              <li className="text-primary-foreground leading-6 font-bold">
+                15+ Publications Showcasing Innovation & Impact
+              </li>
+              <li className="text-primary-foreground leading-6 font-bold">
+                1 US Patent Pending – Pioneering Breakthroughs in the Field
+              </li>
+              <li className="text-primary-foreground leading-6 font-bold">
+                Backed by multimillion dollar grants
+              </li>
+            </ul>
+            <div className="relative flex justify-end">
+              <Image
+                src={"/blob.svg"}
+                alt="R&D / Innovations"
+                width={0}
+                height={0}
+                loading="lazy"
+                layout="responsive"
+                className="max-w-[422px] max-h-[312px] pointer-events-none"
+              />
+              <Image
+                src={"/lady.svg"}
+                alt="R&D / Innovations"
+                width={0}
+                height={0}
+                loading="lazy"
+                layout="responsive"
+                className="max-w-[307px] max-h-[292px] z-20 absolute pointer-events-none"
+              />
+              <Image
+                src={"/lady-fill.svg"}
+                alt="R&D / Innovations"
+                width={0}
+                height={0}
+                loading="lazy"
+                layout="responsive"
+                className="max-w-[266px] max-h-[260px] z-10 absolute pointer-events-none"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-5 bg-[#4A6758] px-10 pb-5 rounded-md overflow-hidden">
+            <Image
+              src={"/teamwork.svg"}
+              alt="Industry Integration"
+              width={0}
+              height={0}
+              loading="lazy"
+              layout="responsive"
+              className="max-w-[424px] pointer-events-none"
+            />
+            <Typography
+              variant={"h5"}
+              className="font-black font-playfair text-primary-foreground text-center -mt-14"
+            >
+              Industry Integration
+            </Typography>
+            <Typography
+              variant={"p"}
+              className=" text-center text-primary-foreground max-w-2/3 "
+            >
+              Providing solutions for leading Fortune 500 global Pharmaceutical
+              companies
+            </Typography>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </div>
+  );
+};
+
+export default Strategy;
--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,6 +1,10 @@
 import Challenges from "./_components/challenges/Challenges";
 import Hero from "./_components/hero/Hero";
 import Solution from "./_components/solution/Solution";
+import LayoutWrapper from "../../components/wrappers/LayoutWrapper";
+import { Typography } from "@/components/ui/typography";
+import { Button } from "@/components/ui/button";
+import Strategy from "./_components/strategy/Strategy";
 
 export default function Home() {
   return (
@@ -8,6 +12,21 @@ export default function Home() {
       <Hero />
       <Challenges />
       <Solution />
+
+      <Strategy />
+      <LayoutWrapper className="text-center lg:py-14 gap-5">
+        <Typography variant={"h3"} className="font-bold max-w-prose">
+          Ready to build a{" "}
+          <span className="text-primary italic font-playfair">simulation</span>{" "}
+          of <br></br> your manufacturing network?
+        </Typography>
+        <Typography variant={"p"} className="max-w-prose">
+          Take the next step in transforming your business to be more
+          sustainable, reduce waste, and optimize your operations for a cleaner,
+          greener future.
+        </Typography>
+        <Button size={"xl"}>Request a demo</Button>
+      </LayoutWrapper>
     </>
   );
 }
