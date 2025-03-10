35d07dfcea096c3b6cf38882deaa906546838336
Code-X-Rahul <rr611305@gmail.com>
2025-03-10 16:42:29 +0530

    chore: unused imnports removed

--- app/(landingPage)/_components/hero/Hero.tsx
+++ app/(landingPage)/_components/hero/Hero.tsx
@@ -1,12 +1,9 @@
-import LayoutWrapper from "@/components/wrappers/LayoutWrapper";
 import HeroCarousel from "./HeroCarousel";
 
 const Hero = () => {
   return (
     <div className="relative grid-bg">
-      {/* <LayoutWrapper className="relative px-0"> */}
       <HeroCarousel />
-      {/* </LayoutWrapper> */}
     </div>
   );
 };
